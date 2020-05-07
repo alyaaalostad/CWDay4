84bdcbfa030bd61841b257cb40c2388fe5017233
Alyaa AlOstad <alyaaalostad@hotmail.com>
2020-05-07 22:35:41 +0300

    Initial Commit

---
+++ ClassWorkDay4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ClassWorkDay4
+//
+//  Created by Alyaa AlOstad on 5/7/20.
+//  Copyright © 2020 Alyaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
